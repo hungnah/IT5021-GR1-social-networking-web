9e96e5a595021df9068ad78b0fa8a59428dedc82
hungnah <nxthanhhung@gmail.com>
2026-05-30 16:39:45 +0700

    fix: enable synchronize to create DB tables

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import { UsersModule } from './users/users.module';
         password: configService.get<string>('DB_PASSWORD', ''),
         database: configService.get<string>('DB_DATABASE', 'feedme'),
         autoLoadEntities: true,
-        synchronize: false,
+        synchronize: true,
       }),
     }),
     UsersModule,
